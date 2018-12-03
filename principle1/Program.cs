52cd0d1a2aa305b15b568669aeb8e483888d4320
Keshav Chaurasia <keshav.chaurasia1995@gmail.com>
2018-12-03 09:23:43 +0545

    Solid principle 1

---
+++ principle1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace dotnet
+{
+    public class Journal
+    {
+        private readonly static List<string> entries = new List<string>();
+        private static int count;
+
+        public int AddEntry(string entry){
+            entries.Add($"{++count}: {entry}");
+            return count;
+        }
+
+        public void RemoveEntry(int index){
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString(){
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false){
+            if( overwrite || !File.Exists(filename)){
+                File.WriteAllText(filename,journal.ToString());
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal j = new Journal();
+            j.AddEntry("Went to the doctor!!");
+            j.AddEntry("Have a kitkat break");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\Users\KeshavChaurasia\Desktop\codes\dotnet\journal.txt";
+            p.SaveToFile(j, filename);
+        }
+    }
+}
---
+++ principle2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+// SOLID Princple 2: Open-Close Principle
+// The Classes should be open for extension by closed for modification
+namespace principle2
+{
+    public enum Color {
+        Green,Red,Blue
+    }
+
+    public enum Size{
+        Small,Medium,Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size){
+            foreach(var p in products)
+                if(p.Size == size)
+                    yield return p;
+        }
+        // you see the productfilter we need to modify it everytime we need new type of filters.. this is bad and we should not do this cause we need to modify.. This should be closed
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color){
+            foreach(var p in products)
+                if(p.Color == color)
+                    yield return p;
+        }
+        //you see everytime I need different filter I need to modify the class.. this is bad. this is very bad
+        public IEnumerable<Product> FilterByColorAndSize(IEnumerable<Product> products, Color color, Size size){
+            foreach(var p in products)
+                if(p.Color == color && p.Size == size)
+                    yield return p;
+        }
+
+
+    }
+
+    //So lets extend this and make it better and by creating generic filter.. we are open to this..
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied();
+    }
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple",Color.Green,Size.Small);
+            var tree = new Product("Tree",Color.Green,Size.Large);
+            var bread = new Product("Apple",Color.Blue,Size.Medium);
+            var house = new Product("House",Color.Blue,Size.Large);
+
+            Product[] products = {apple,tree,bread,house};
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Products (old):");
+            foreach(var p in pf.FilterByColor(products,Color.Green))
+            {
+                Console.WriteLine($" - {p.Name}");
+            }
+            
+        }
+    }
+}
