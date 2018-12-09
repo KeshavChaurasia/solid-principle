9912cb9683c64aba6c5aa9643648c1c8f407aa02
Keshav Chaurasia <keshav.chaurasia1995@gmail.com>
2018-12-09 08:07:11 +0545

    Principle 3 added

--- principle2/Program.cs
+++ principle2/Program.cs
@@ -49,13 +49,76 @@ namespace principle2
     }
 
     //So lets extend this and make it better and by creating generic filter.. we are open to this..
+    //Ispecification implements specification pattern that whether or not certain criteria is implemented or not 
     public interface ISpecification<T>
     {
-        bool IsSatisfied();
+        bool IsSatisfied(T t);
+    }
+
+    //this ifilter filters from items with T type of specs and returne IEnumerable<T>
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specs);
+    }
+
+    //so lets make color specification
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == this.color;
+        }
+    }
+
+    //this size specification takes in Product and checks if the size is satisfied by the condition provided in the constructor
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpecification(Size size){
+            this.size = size;
+        }
+        
+        public bool IsSatisfied(Product t){
+            return t.Size == this.size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second){
+            this.first = first ?? throw new ArgumentNullException(paramName:nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName:nameof(second));
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t); 
+        }
+    }
+
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specs)
+        {
+            foreach(var i in items){
+                if(specs.IsSatisfied(i)){
+                    yield return i;
+                }
+            }
+        }
     }
 
 
 
+    // finally  we shouldn't be able to modify the class that's already built but add more class and make it more extensible 
     public class Program
     {
         static void Main(string[] args)
@@ -72,7 +135,19 @@ namespace principle2
             {
                 Console.WriteLine($" - {p.Name}");
             }
-            
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green Products (new):");
+            foreach(var p in bf.Filter(products,new ColorSpecification(Color.Green))){
+                Console.WriteLine($" - {p.Name}");
+            }
+
+            Console.WriteLine("Large and blue (new):");
+            foreach(var p in bf.Filter(products, new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))){
+                Console.WriteLine($" - {p.Name}");
+
+            }
+
         }
     }
 }
---
+++ principle3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+//SOLID Principle 3
+// Liskov Substitution Principle : you should be able to substitute a base type for a sub type 
+
+namespace principle3
+{
+    public class Rectangle
+    {
+        public virtual int Width{get;set;}
+        public virtual int Height{get;set;}
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public override string ToString(){
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle{
+        public override int Width 
+        {
+            set {base.Width = base.Height = value; }
+        }
+        public override int Height {
+            set {base.Height = base.Width = value;}
+        }
+
+    }
+    public class Program
+    {
+        public static int Area(Rectangle r) => r.Height * r.Width ; 
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 5;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+
+        }
+    }
+}
